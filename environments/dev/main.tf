13175217163e9a06a5cfe8e2c971cea573d848fe
venky <snikommuvenky115@gmail.com>
2024-08-23 01:06:21 +0100

    Updated tfvars file

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source = "git::https://github.com/venky1912/terraform-modules.git//aws/modules/vpc?ref=main"
     
   # Variables required by the network module
-  cidr_block = var.vpc_cidr
+  vpc_cidr = var.vpc_cidr
   public_subnets = var.public_subnets
   private_subnets = var.private_subnets
   region         = var.region
