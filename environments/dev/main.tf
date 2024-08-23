6f5494a3f7e6b352aef7c089cb09cfd125ca15d2
venky <snikommuvenky115@gmail.com>
2024-08-23 01:00:02 +0100

    changes in VPC

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,9 +2,8 @@ module "vpc" {
     source = "git::https://github.com/venky1912/terraform-modules.git//aws/modules/vpc?ref=main"
     
   # Variables required by the network module
-  vpc_cidr_block = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr
   public_subnets = var.public_subnets
   private_subnets = var.private_subnets
   region         = var.region
-  environment    = var.environment
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,4 @@
-variable "vpc_cidr_block" {
+variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
